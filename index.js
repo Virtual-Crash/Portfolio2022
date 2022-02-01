13e52f3567af45047220244c558945475821dcbc
Kayla Jeter <Kay-Rose-Marie@outlook.com>
2022-01-31 20:33:44 -0800

    added pug updated index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 3000
 
+//view engine to show html and join pages
 express()
+  .use(express.static(path_join(__dirname, 'pages')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'pug')
   .get('/', (req, res) => res.render('pages/Home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
