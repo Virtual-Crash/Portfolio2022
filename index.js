38aa72e85f1061ceedffad16fd3b1cbfb117ac4d
Kayla Jeter <Kay-Rose-Marie@outlook.com>
2022-01-31 20:56:35 -0800

    everything is on fire

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000
 
 //view engine to show html and join pages
 express()
-  .use(express.static(path_join(__dirname, 'pages')))
+  .use(express.static(path.join(__dirname, 'pages')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'pug')
-  .get('/', (req, res) => res.render('pages/Home'))
+  .get('/', (req, res) => res.render('\Home'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
